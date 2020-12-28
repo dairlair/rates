f7cd7b6c03864fe879294e8a658c8eb85fb85c07
Alexander Emelianov <alexander.emelianov@exinity.com>
2020-12-28 10:19:18 +0200

    Converter service updated

--- frontend/src/app/services/converter.service.ts
+++ frontend/src/app/services/converter.service.ts
@@ -9,33 +9,44 @@ export class ConverterService {
   constructor(private rates: Rate[]) {
   }
 
-  public convert(baseCurrencyId, quoteCurrencyId: number): Rate[]
+  public convert(baseCurrencyId, quoteCurrencyId: number, depth: number = 0): Rate[]
   {
-    let conversion: Rate[] = [];
-    // Check the direct rates (base currency and quote currency the same as required)
-    this.rates.forEach((rate: Rate) => {
-      if (rate.base_currency.id == baseCurrencyId && rate.quote_currency.id == quoteCurrencyId) {
-        conversion.push(rate);
-        return conversion;
-      }
-      if (rate.quote_currency.id == baseCurrencyId && rate.base_currency.id == quoteCurrencyId) {
-        conversion.push(ConverterService.revertRate(rate));
-        return conversion;
-      }
-    });
-
-    // No way for direct conversion, lets try to find other way, using recursion
-    this.rates.forEach((rate: Rate) => {
-      if (rate.base_currency.id == baseCurrencyId) {
-        let crossConversion: Rate[] = this.convert(rate.quote_currency.id, quoteCurrencyId);
-        if (crossConversion.length > 0) {
-          console.log('Cross conversion: ', crossConversion);
-          conversion = conversion.concat([rate], crossConversion);
+    for (let i in this.rates) {
+      const directRate = this.rates[i];
+      const revertedRate = ConverterService.revertRate(directRate);
+      // Check the direct/reverted rates (base currency and quote currency the same as required)
+      const consideredRates = [directRate, revertedRate];
+      for (let j in consideredRates) {
+        const rate = consideredRates[j];
+        if (ConverterService.isSuiteRate(rate, baseCurrencyId, quoteCurrencyId)) {
+          return [rate];
         }
       }
-    });
+    }
 
-    return conversion;
+    // No way for direct conversion, lets try to find other way, using recursion
+    // Should be moved to settings.
+    if (depth > 3) {
+      return [];
+    }
+
+    for (let i in this.rates) {
+      const directRate = this.rates[i];
+      const revertedRate = ConverterService.revertRate(directRate);
+      // Check the direct/reverted rates (base currency and quote currency the same as required)
+      const consideredRates = [directRate, revertedRate];
+      for (let j in consideredRates) {
+        const rate = consideredRates[j];
+        if (rate.base_currency.id == baseCurrencyId) {
+          let crossConversion: Rate[] = this.convert(rate.quote_currency.id, quoteCurrencyId, depth + 1);
+          if (crossConversion.length > 0) {
+            return [].concat([rate], crossConversion);
+          }
+        }
+      }
+    }
+
+    return [];
   }
 
   private static revertRate(rate: Rate): Rate
@@ -47,5 +58,10 @@ export class ConverterService {
     reveredRate.rate = 1 / rate.rate;
 
     return reveredRate;
+  }
+
+  private static isSuiteRate(rate: Rate, baseCurrencyId, quoteCurrencyId: number): boolean
+  {
+    return rate.base_currency.id == baseCurrencyId && rate.quote_currency.id == quoteCurrencyId;
   }
 }
